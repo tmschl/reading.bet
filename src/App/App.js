fc998fbb929bbae90c3e0ba01905b14617b8bdd2
timmy <timothy.schiller@gmail.com>
2023-04-27 12:38:59 -0400

    mock login route; mock user data

--- src/App/App.js
+++ src/App/App.js
@@ -7,12 +7,9 @@ import { User } from '../User/User';
 export function App(props) {
   return (
     <div className="app" >
-      <header>
-        <div>user profile</div>
-      </header>
       <h1>{props.name}</h1>
       <User />
-      <SearchBar />
+      {/* <SearchBar /> */}
     </div>
   );
 }
--- src/User/User.js
+++ src/User/User.js
@@ -1,10 +1,47 @@
-
+import { useState } from 'react';
+import { SearchBar } from '../SearchBar/SearchBar';
 
 export function User () {
-    console.log('hi');
-    return (
-        <div className="user">
-            <div>user</div>
-        </div>
-    );
+	const [user, setUserState] = useState({
+		username: "",
+		name: "",
+		password: "",
+		isLoggedIn: false,
+		readingNow: []
+	});
+
+	return (
+		<div className="user">
+			<form>
+				<label htmlFor="login">
+					Login
+					<input
+							id="username"
+							value={user.username}
+							placeholder="email"
+							onChange={(e) => setUserState({
+								...user, 
+								username: `${e.target.value}`
+							})}
+
+					/>
+				</label>
+				<label htmlFor="login">
+					Password 
+					<input
+							id="password"
+							value={user.password}
+							placeholder="password"
+							onChange={(e) => setUserState({
+								...user, 
+								password: `${e.target.value}`,
+								isLoggedIn: true
+
+							})}
+							/>
+				</label>
+			</form>
+			{ user.isLoggedIn ? <SearchBar /> : "" }
+		</div>
+	);
 }
